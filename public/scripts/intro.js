e997f099f7268352c6580377d9dcd801a5fc2579
Johnes Thomas <johnes_brasil10@hotmail.com>
2020-06-07 01:20:24 -0300

    first commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,70 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+
+// let myvar = "Hello"
+// const myconst = "He"
+
+// document.write (myvar)
+
+// string "Isso é uma string"
+// 'isso tambem é uma string'
+// const s1 = "Isso é uma string
+
+// number
+// const n1 = 1
+// const n2 = 12
+
+// boolean = true ou false
+// const bTrue = true
+// cont bFalse = false
+
+// objeto possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//        return "run forest"
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+// Array = vetor
+// const colecaodebolinhas = ["blue", "green", 1, {name: "My name"}]
+// document.write(colecaodebolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+//registrar função
+// function sayMyName(){
+//   document.write("Johnes")
+// }
+// //executar
+// sayMyName()
+
+// function sayMyName(name){
+//   document.write(name)
+// }
+// //executar
+// sayMyName("Johnes")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ){
+//   document.write ("Reprovado")
+// } else {
+//     document.write ("Aprovado")
+
+// }
+
+
+//loop refetições
+
+// for (i = 0; i < 10; i++){
+//   document.write("<p> alo </p>")
+// }
